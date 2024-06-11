3d02d4654a2372af7a806f74fdefcdb8e0ea19cd
525649253 <525649253@qq.com>
2024-06-11 16:52:55 +0800

    link "Game" 改为 “Demo”

--- layout/common/navbar.jsx
+++ layout/common/navbar.jsx
@@ -57,7 +57,7 @@ class Navbar extends Component {
                         {Object.keys(links).length ? <Fragment>
                             {Object.keys(links).map(name => {
                                 const link = links[name];
-                                const target = name == 'Game' ? '_self' : '_blank';
+                                const target = name == 'Demo' ? '_self' : '_blank';
                                 return <a class="navbar-item" target={target} rel="noopener" title={name} href={link.url}>
                                     {link.icon ? <i class={link.icon}></i> : name}
                                 </a>;
